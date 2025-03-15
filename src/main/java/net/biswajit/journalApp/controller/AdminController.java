8d6bc118b9d09bd028097f675fd7853cdfb6e615
Biswajit Biswas <bbiswajit1999sh@gmail.com>
2025-03-16 03:37:11 +0530

    Add: Role based Auth

---
+++ src/main/java/net/biswajit/journalApp/controller/AdminController.java
@@ -0,0 +1,38 @@
+package net.biswajit.journalApp.controller;
+
+import net.biswajit.journalApp.entity.User;
+import net.biswajit.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public ResponseEntity<?> getAllUsers(){
+        List<User> list = userService.getAllUser();
+        if(!list.isEmpty()){
+            return new ResponseEntity<>(list, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/create-user-admin")
+    public ResponseEntity<?> createUserAdmin(@RequestBody User user){
+        try {
+            userService.saveAdmin(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }catch (Exception e){
+            return new ResponseEntity<>("Failed to Create Admin "+e, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
