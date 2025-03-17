6c026d7cbc95ac88de354918377aeea5e19dda36
Biswajit Biswas <bbiswajit1999sh@gmail.com>
2025-03-17 17:21:15 +0530

    Add: Java Mail Service

---
+++ src/test/java/net/biswajit/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,21 @@
+package net.biswajit.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    public void test(){
+        emailService.sendEmail(
+                "bbiswajit1999sh@gmail.com",
+                "Greetings",
+                "Hello, Brother how r u ??"
+        );
+    }
+}
